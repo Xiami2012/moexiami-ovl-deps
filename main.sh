eae388bec2f4bb0475a77210c4765504766c89d5
Xiami <i@f2light.com>
2024-03-08 19:08:26 +0759

    actions: Add versions of tools we use for reproduciblity
    In NEWS of tar-1.35:
    
    * Leave the devmajor and devminor fields empty (rather than zero) for
      non-special files, as this is more compatible with traditional tar.
    
    This makes archiving same contents via tar-1.34 and tar-1.35 different.

--- main.sh
+++ main.sh
@@ -13,7 +13,23 @@ fi
 FILES=()
 source gen-go-deps.sh
 
-relnote="\`\`\`
-$(sha256sum "${FILES[@]}")
+relnote="## Information for reproducible builds
+
+OS:
+\`\`\`
+$(grep -E '^(NAME|VERSION)=' /etc/os-release)
+\`\`\`
+
+\`\`\`
+$(go version)
+
+$(tar --version | head -1)
+
+$(xz --version)
+\`\`\`
+
+## Checksum of packaged files
+\`\`\`
+$(sha256sum --tag "${FILES[@]}")
 \`\`\`"
 gh release create "${P}" --target "${GITHUB_SHA}" -n "${relnote}" ${FILES[@]}
